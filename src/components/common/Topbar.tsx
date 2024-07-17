25d2c3f30a761661be2276551197c5f7bf8a928c
goldipl <mgodlewski92@gmail.com>
2024-07-17 20:55:55 +0200

    delete React.FC and change topbar notice

--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -5,7 +5,7 @@ interface TopbarProps {
   onClose: () => void;
 }
 
-const Topbar: React.FC<TopbarProps> = ({ onClose }) => {
+const Topbar = ({ onClose }: TopbarProps) => {
   const [isHidden, setIsHidden] = useState(false);
 
   const handleClose = () => {
@@ -17,13 +17,15 @@ const Topbar: React.FC<TopbarProps> = ({ onClose }) => {
     <div id="topbar" className={`topbar ${isHidden ? "hidden" : ""}`}>
       <div className="topbar-wrapper">
         <p className="topbar-wrapper__text">
-          <strong>Ważny komunikat!</strong>
+          <strong>Important Notice!</strong>
           <span className="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et
-            nibh ante. Nam tincidunt urna eget nulla.
+            Due to the extremely dry weather conditions, there is a high risk of
+            fire in the forests. Please avoid any activities that could
+            potentially start a fire, such as campfires or using fireworks.
+            Report any signs of fire to the authorities immediately.
           </span>
           <span className="topbar-wrapper__close-icon" onClick={handleClose}>
-            <img src={closeIcon.src} alt="Zamknij" width={14} height={14} />
+            <img src={closeIcon.src} alt="Close" width={14} height={14} />
           </span>
         </p>
       </div>
